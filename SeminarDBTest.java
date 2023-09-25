743f605b825fc2b767a1ca2038ce0a0fffaa2e2b
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-25 19:24:39 -0400

    Update SeminarDBTest.java

--- SeminarDBTest.java
+++ SeminarDBTest.java
@@ -1,4 +1,6 @@
 import student.TestCase;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class SeminarDBTest extends TestCase{
 
@@ -33,6 +35,24 @@ public class SeminarDBTest extends TestCase{
     public void testDateInsert() {
         seminarDB.DateInsert("20230924", seminar);
         assertEquals(2, seminarDB.getDateNodeCount());
+    }
+    
+    public void testIdPrint() {
+        SeminarDB seminarDB = new SeminarDB(10); // Initialize SeminarDB object with worldSize 10
+        Seminar seminar = new Seminar(1, "Test", "Test Location", 100, (short) 1, (short) 1, 10, new String[]{"TestWord"}, "Test Desc"); // Initialize a Seminar object
+        
+        // Capture the output to the console
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        
+        // Call IdPrint method
+        seminarDB.IdPrint("key1", seminar);
+        
+        // Assert whether the printed content is as expected
+        assertEquals("This tree is empty\n", outContent.toString());
+        
+        // Optionally, reset System.out to its original state after we are done
+        System.setOut(System.out);
     }
     
     // Similar tests can be written for CostInsert and DateInsert depending on how you want to validate them
