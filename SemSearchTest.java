c521b05d28a42d48acac15cb6daafb5474292fdc
Anshpvt <132394937+Anshpvt@users.noreply.github.com>
2023-09-25 14:54:01 -0400

    Update SemSearchTest.java

--- SemSearchTest.java
+++ SemSearchTest.java
@@ -22,5 +22,27 @@ public class SemSearchTest extends TestCase {
         assertNotNull(sem);
         SemSearch.main(null);
     }
+
+    /**
+     * Test with the first argument not being a power of 2.
+     */
+    public void testMainInvalidWorldSize() {
+        String[] args = {"3", "someString"};
+        SemSearch.main(args);
+        // assert the output, it should be "invalid world size"
+        String expected = "invalid world size\n";
+        assertEquals(expected, systemOut().getHistory());
+    }
+
+    /**
+     * Test with the first argument being a power of 2 and valid second argument.
+     */
+    public void testMainValidArgs() {
+        String[] args = {"4", "someString"};
+        SemSearch.main(args);
+        // assert the output, it should be "com called"
+        String expected = "com called\n";
+        assertEquals(expected, systemOut().getHistory());
+    }
 }
 
